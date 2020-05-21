5e17419c40ff5e6442a0296a4d6111d7463b9ca8
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-21 13:37:09 +0100

    Disabled ARC

---
+++ ContactsMRC/ContactsMRC/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContactsMRC
+//
+//  Created by Tobi Kuyoro on 21/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
