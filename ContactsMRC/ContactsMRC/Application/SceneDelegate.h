4e56e3a74c88d5a05d2b8eab5505744e15872d01
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-21 13:36:30 +0100

    Initial commit

---
+++ ContactsMRC/ContactsMRC/Application/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ContactsMRC
+//
+//  Created by Tobi Kuyoro on 21/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
